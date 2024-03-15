61d434bb847199732c5a27734128b70d1c720e52
Maja Głowacz <127887303+Kochamkajaka@users.noreply.github.com>
2024-03-15 23:16:01 +0100

    CYCE KURWA MAĆ ZAJEBIE SIĘĘ

---
+++ ULUNGI_Gra/Assets/point.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class point : MonoBehaviour
+{
+    public Text scoreText;
+    public Text highscoreText;
+    int score = 0;
+    int highscore = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
