8ed9a614370cbb694fa97c5740284055182f3632
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2024-03-15 19:51:37 +0100

    zycie

--- ULUNGI_Gra/Assets/Skrypty/ScriptsPatryka/Slowtime.cs
+++ ULUNGI_Gra/Assets/Skrypty/ScriptsPatryka/Slowtime.cs
@@ -4,11 +4,12 @@ using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.GraphicsBuffer;
 
 public class Slowtime : MonoBehaviour
 {
     private float cooldown;
-    public Transform target;
+ //   public Transform target;
     Finalmovement move;
     Rigidbody2D rb;
     private bool timeslowed = false;
@@ -17,21 +18,36 @@ public class Slowtime : MonoBehaviour
     void Start()
     {
         move= GameObject.Find("Player").GetComponent<Finalmovement>();
-        Background = GameObject.Find("Background").GetComponent<Image>();
+      //  Background = GameObject.Find("Background").GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
         cooldown -= Time.deltaTime; 
-        if(cooldown <= 0 && timeslowed && move.m_Grounded)
-        {
+      //  if(cooldown <= 0 && timeslowed && move.m_Grounded)
+      //  {
+        //    Time.timeScale = 1f;
+       //     move.runSpeed = move.startspeed;
+       //     move.m_JumpForce = move.startjumpforce;
+       //     move.rb.gravityScale = 1f;
+       //     timeslowed = false ;
+            //Background.color = new Color(0,0,0,0f);
+      //  }
+        if(Input.GetKeyUp(KeyCode.Q) && timeslowed==false) {
+            Time.timeScale = 0.5f;
+            cooldown = 10;
+          //  transform.position = target.position;
+            move.runSpeed = move.doublespeed;
+            move.rb.gravityScale = 10f;
+            move.m_JumpForce = move.slowedjumpforce;
+            timeslowed = true;
+        }else if(Input.GetKeyUp(KeyCode.Q) && timeslowed==true) {
             Time.timeScale = 1f;
             move.runSpeed = move.startspeed;
             move.m_JumpForce = move.startjumpforce;
-            move.rb.gravityScale = 3f;
-            timeslowed = false ;
-            Background.color = new Color(0,0,0,0f);
+            move.rb.gravityScale = 1f;
+            timeslowed = false;
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -40,12 +56,12 @@ public class Slowtime : MonoBehaviour
         {
             Time.timeScale = 0.5f;
             cooldown = 10;
-            transform.position = target.position;
+           // transform.position = target.position;
             move.runSpeed = move.doublespeed;
             move.rb.gravityScale = 10f;
             move.m_JumpForce = move.slowedjumpforce;
             timeslowed = true;
-            Background.color = new Color(0,0,1,0.5f);
+          //  Background.color = new Color(0,0,1,0.5f);
         }
     }
 }
